c44c2c332fd91fdb64fee76209d6141691795bd4
Rémi DEMIR <demir.remi@gmail.com>
2021-08-18 22:28:45 +0200

    add ListMentors component with redux connected with API

--- front/src/components/ListRessources/index.js
+++ front/src/components/ListRessources/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Card from 'src/components/ListRessources/Card';
 // import Filter from 'src/components/ListRessources/Filter';
-// import SearchBar from 'src/components/ListRessources/SearchBar';
 import './styles.scss';
 
 const ListRessources = ({ ressources }) => (
--- front/src/containers/App/index.js
+++ front/src/containers/App/index.js
@@ -1,12 +1,16 @@
 import { connect } from 'react-redux';
 
 import { createGetRessourcesAction } from 'src/actions/ressources';
+import { createGetMentorsAction } from 'src/actions/mentors';
 
 import App from 'src/components/App';
 
 const mapDispatchToProps = (dispatch) => ({
   getRessources: () => {
     dispatch(createGetRessourcesAction());
+  },
+  getMentors: () => {
+    dispatch(createGetMentorsAction());
   },
 });
 
