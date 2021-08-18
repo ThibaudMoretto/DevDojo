8892b1a13fa8d3e5dac87b68e504f1d1a85a58d5
Michel Vieira <michelvieira.pro@gmail.com>
2021-08-18 22:26:18 +0200

    Added a little css  and text for better understanding

--- front/src/components/FicheRessource/index.js
+++ front/src/components/FicheRessource/index.js
@@ -7,14 +7,16 @@ function FicheRessource({ ressource }) {
     <div>
       <div className="container">
       <div className="title">{ressource.title}</div>
-      <div className="author">Publié par Grafikart</div>
+      <div className="author">Publié par un auteur</div>
       <div className="date">{ressource.publication_date}</div>
       <div className="description">{ressource.description}</div>
-      <div className="techno">{ressource.language_id}</div>
-      <div className="duration">{ressource.duration}</div>
-      <div className="difficulty">{ressource.difficulty_id}</div>
-      <div className="category">{ressource.ressource_type_id}</div>
-      <div className="link">{ressource.link}</div>
+      <div className="techno">JavaScript</div>
+      <div className="duration">Durée : {ressource.duration}min</div>
+      <div className="difficulty">Difficulté : {ressource.difficulty_id}/5</div>
+      <div className="category">Type de ressource : {ressource.ressource_type_id}</div>
+      <div className="link">
+        <a href="">Accéder à la ressource</a>
+      </div>
       <div className="btn">
         <button className="edit">Edit</button>
         <button className="delete">Delete</button>
