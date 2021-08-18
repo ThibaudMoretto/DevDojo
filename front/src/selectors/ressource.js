793e7c93e3b59984647c5e90d4388a4850745c40
Michel Vieira <80683541+VIEIRA-Michel@users.noreply.github.com>
2021-08-18 22:57:59 +0200

    Update ressource.js

--- front/src/selectors/ressource.js
+++ front/src/selectors/ressource.js
@@ -3,9 +3,9 @@
 
 /**
  *  on trouve la recette voulue dans la liste des recettes
- * @param {Array} ressources - toutes les recettes
- * @param {string} searchedId - le slug de la recette recherchée
- * @return {Object} - La recette trouvée
+ * @param {Array} ressources - toutes les ressources
+ * @param {string} searchedId - le link de la ressource recherchée
+ * @return {Object} - La ressource trouvée
  */
 export function findRessource(ressources, searchedLink) {
   const ressource = ressources.find((testedRessource) => {
