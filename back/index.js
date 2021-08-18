9a71b2de9b2e6bde24cd7bc2b25204d6f0c6e290
ThibaudMoretto <thibaud.moretto@gmail.com>
2021-08-18 14:29:14 +0200

    feat: add cors handling

--- back/index.js
+++ back/index.js
@@ -4,11 +4,17 @@ const express = require('express');
 const app = express();
 const apiRouter = require('./app/routers/api');
 const sanitizer = require('sanitizer');
+const cors = require('cors');
 const port = process.env.PORT || `11000`;
 
 //A utiliser pour le POST
 app.use(express.urlencoded({ extended: true }));
 
+//A utiliser pour passer du JSON dans le body
+app.use(express.json());
+
+//On autorise tout le monde à venir sur notre API
+app.use(cors());
 
 // Pour prévenir des failles XSS, on met en place un sanitizer qui permettra d'empêcher l'utilisateur de saisir du HTML
 app.use((request, _, next) => {
