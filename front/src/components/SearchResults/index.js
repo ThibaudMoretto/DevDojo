d016f176dfbbe65b5df2e1a78bf1007fd3fbe57c
Rémi DEMIR <demir.remi@gmail.com>
2021-08-19 20:55:12 +0200

    Update index.js

--- front/src/components/SearchResults/index.js
+++ front/src/components/SearchResults/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ListMentors from 'src/containers/ListMentors'
 import ListRessources from 'src/containers/ListRessources'
-import Header from 'src/components/Header';
-import Footer from 'src/components/Footer';
 
 import './styles.scss';
 
