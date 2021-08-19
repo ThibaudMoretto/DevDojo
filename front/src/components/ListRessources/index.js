3100ad8b6a555e9e5ad70718d7171412008254fc
Rémi DEMIR <demir.remi@gmail.com>
2021-08-19 20:45:02 +0200

    add search-results and searchBar feature

--- front/src/components/ListRessources/index.js
+++ front/src/components/ListRessources/index.js
@@ -3,15 +3,57 @@ import Card from 'src/components/ListRessources/Card';
 // import Filter from 'src/components/ListRessources/Filter';
 import './styles.scss';
 
-const ListRessources = ({ ressources }) => (
-  <div>
-    {ressources.map((ressource) => (
-      <Card
-        key={ressource.id}
-        {...ressource}
-      />
-    ))}
-  </div>
-);
+const ListRessources = ({ ressources }) => {
+
+  console.log(ressources)
+
+  return (
+    <div>
+      {ressources.map((ressource) => (
+        <Card
+          key={ressource.id}
+          {...ressource}
+        />
+      ))}
+    </div>
+  )
+
+};
 
 export default ListRessources;
+    // <div>
+    //   {ressources.filter(function => name.includes('J')).map(filteredName => (
+    //     <li>
+    //       {filteredName}
+    //     </li>
+    //   ))}
+    // </div>
+
+  // <div>
+  //   {ressources.map((ressource) => (
+  //     <Card
+  //       key={ressource.id}
+  //       {...ressource}
+  //     />
+  //   ))}
+  // </div>
+/**
+ *
+ *
+ *
+ * {this.state.tasks
+        .filter(function(task, index, props) {
+          return task.completed === props.completedTasks;
+        })
+        .map(function(task, index) {
+          return(
+            <Task
+              name={task.name}
+              key={task.id}
+              completed={task.completed}
+              onCompleted={function() {this.onCompletedTask(index)}.bind(this)}
+              onRemove={function() {this.onRemoveTask(index)}.bind(this)}
+            />
+          );
+        }.bind(this))}
+ */
