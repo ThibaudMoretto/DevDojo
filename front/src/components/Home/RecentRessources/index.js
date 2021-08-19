9a3d4887a099b3d0760770fd2b97423e048167c8
François STABLE <nansouze@gmail.com>
2021-08-19 12:20:12 +0200

    add section ressources

--- front/src/components/Home/RecentRessources/index.js
+++ front/src/components/Home/RecentRessources/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './styles.scss';
+import cardImg from '../../../assets/images/Card_Img.png'
 
 function RecentRessources() {
   return (
-    <div>
-      RecentRessources
+    <div className="lastRessources">
+      <h2>Les dernières <span>Ressources</span></h2>
+      <div className="lastRessources-card">
+        <div className="lastRessources-card-img">
+          <img src={cardImg} alt="Apprendre le HTML" />
+        </div>
+        <div className="lastRessources-card-content">
+          <h3>Apprendre le HTML</h3>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed blandit nisi.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
