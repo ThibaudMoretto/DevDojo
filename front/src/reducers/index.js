250aaa55bc872d0e91a788d27eea26fc79f29a96
Rémi DEMIR <demir.remi@gmail.com>
2021-08-19 15:31:45 +0200

    setup searchResult component + searchBar reducer

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import ressourcesReducer from './ressources';
 import mentorsReducer from './mentors';
+import searchBarReducer from './searchBar';
 
 const rootReducer = combineReducers({
   ressources: ressourcesReducer,
   mentors: mentorsReducer,
+  searchBar: searchBarReducer,
 });
 
 export default rootReducer;
