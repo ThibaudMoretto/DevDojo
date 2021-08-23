fc00ed27c62d00937d99be9113598a5a9db12bb5
Rémi DEMIR <demir.remi@gmail.com>
2021-08-23 23:30:07 +0200

    fixed css fiche components

--- front/src/components/FicheMentor/index.js
+++ front/src/components/FicheMentor/index.js
@@ -22,16 +22,14 @@ function FicheMentor({ mentor, isLogged }) {
       </div>
 
       <div className="mentor">
-        <div className="mentor--name">Nom : {mentor.name}</div>
-        <div className="mentor--description">Présentation : {mentor.description}</div>
+        <div className="mentor--name">{mentor.name}</div>
+        <div className="mentor--description">{mentor.description}</div>
         <div className="mentor--github">Github : {mentor.github_account}</div>
         <div className="mentor--youtube">Youtube : {mentor.youtube_account}</div>
-        <div className="mentor--website">Site Web : {mentor.website}</div>
         <div className="mentor--witter">Twitter : {mentor.twitter_account}</div>
-        <div className="mentor--linkedin">Linkedin : {mentor.linkedin_account}</div>
-        <div className="mentor--twitch">Twitch : {mentor.twitch_account}</div>
-        <div className="mentor--ressources"> Mes ressources :</div>
       </div>
+
+      <div className="ressource-title">Ses ressources</div>
 
       <div className="ressources">
         {mentor.ressource.map((ress) => (
