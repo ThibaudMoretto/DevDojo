533ec7c82175aa76bca5b396eea2cde53dcdc544
ThibaudMoretto <thibaud.moretto@gmail.com>
2021-08-24 22:48:53 +0200

    feat: add Joi validation for adding ressource and author features

--- back/app/validations/schemas/ressource.js
+++ back/app/validations/schemas/ressource.js
@@ -3,19 +3,19 @@ const Joi = require('joi');
 // Afin de créer les règles de validation on doit créer un objet de schéma Joi.
 module.exports = Joi.object({
     // A l'intérieur de celui-ci on va définir la liste des propriété possibles, et les règles pour chacune d'elles
-    title: Joi.string().required().required(),
+    title: Joi.string().required(),
     description: Joi.string().required(),
     link: Joi.string().required(),
-    publication_date: Joi.date().required(),
-    duration: Joi.number().required(),
+    publication_date: Joi.string().isoDate().required(),
+    duration: Joi.number().integer().required(),
     is_free: Joi.boolean().required(),
-    difficulty_id: Joi.number().required(),
-    language_id: Joi.number().required(),
-    author_id: Joi.number().required(),
-    ressource_type_id: Joi.number().required(),
+    difficulty_id: Joi.number().integer().required(),
+    language_id: Joi.number().integer().required(),
+    author_id: Joi.number().integer().required(),
+    ressource_type_id: Joi.number().integer().required(),
     //Doit être un tableau contenant zéro ou plusieurs objets composé d'un champ ID qui doit être un number
-    technologiesRelated: Joi.array().items(Joi.object({id: Joi.number})).required(),
-    technologiesRequired: oi.array().items(Joi.object({id: Joi.number})),
+    technologiesRelated: Joi.array().items(Joi.object({id: Joi.number().integer()})).required(),
+    technologiesRequired: Joi.array().items(Joi.object({id: Joi.number().integer()})),
     
     // Il existe une rèle de validation qui dit qu'une propriété doit être obligatoire ou non.
     // Ce type de validation peut être a jouté à l'objet lui-même afin de définir des règles à l'objet entier.
