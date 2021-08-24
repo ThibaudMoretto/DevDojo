e8dde66588073c5b62f0e309202ddbe84a0c9233
ThibaudMoretto <thibaud.moretto@gmail.com>
2021-08-24 17:02:49 +0200

    feat: Refresh token OK

--- index.js
+++ index.js
@@ -11,10 +11,36 @@ const port = process.env.PORT || `11000`;
 app.use(express.json());
 
 //A utiliser pour le POST
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+   extended: true
+}));
 
 //On autorise tout le monde à venir sur notre API
 app.use(cors());
+
+const expressSwagger = require('express-swagger-generator')(app);
+const options = {
+   swaggerDefinition: {
+      info: {
+         description: 'API Dev Dojo',
+         title: 'Dev Dojo API Documentation',
+         version: '1.0.0',
+      },
+      host: 'localhost:11500',
+      // host: 'https://coding--dojo.herokuapp.com/api/v1.0:3000',
+      // basePath: '/api',
+      basePath: '/api/v1.0',
+      produces: [
+         "application/json"
+      ],
+      schemes: ['http']
+      // schemes: ['https']
+   },
+   basedir: __dirname, //Chemin absolu de l'app
+   files: ['./app/routers/*.js']
+};
+//! Attention, l'ordre est toujours important, il faut que le middleware de la doc se trouve avant le routeur de base
+expressSwagger(options);
 
 // Pour prévenir des failles XSS, on met en place un sanitizer qui permettra d'empêcher l'utilisateur de saisir du HTML
 app.use(sanitizer.sanitize);
