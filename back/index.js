f19ec686f0eb463396edf5e29e7adfc954e0aaca
ThibaudMoretto <thibaud.moretto@gmail.com>
2021-08-25 16:44:43 +0200

    feat: add swagger doc

--- back/index.js
+++ back/index.js
@@ -26,15 +26,12 @@ const options = {
          title: 'Dev Dojo API Documentation',
          version: '1.0.0',
       },
-      host: 'localhost:11500',
-      // host: 'https://coding--dojo.herokuapp.com/api/v1.0:3000',
-      // basePath: '/api',
+      host: 'https://coding--dojo.herokuapp.com/api/v1.0:3000',
       basePath: '/api/v1.0',
       produces: [
          "application/json"
       ],
-      schemes: ['http']
-      // schemes: ['https']
+      schemes: ['https']
    },
    basedir: __dirname, //Chemin absolu de l'app
    files: ['./app/routers/*.js']
