541514c81124261841f6b22e62b03019962bee7d
François STABLE <nansouze@gmail.com>
2021-08-31 16:58:10 +0200

    add ressource + mentor cards home page

--- front/src/components/Lists/ListMentors/Card/index.js
+++ front/src/components/Lists/ListMentors/Card/index.js
@@ -3,21 +3,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './styles.scss';
-import image from 'src/assets/images/mentor.jpg';
+import mentor from 'src/assets/images/Avatar_Mentor.png';
 
 const Card = ({ name, description }) => (
-  <article className="card">
-    <div className="card--container">
-      <div className="card-content">
-        {/* Lien vers la fiche mentor avec /ressources/+name */}
-        <Link to={`/mentors/${name}`}>
-          <img src={image} alt="une de image mentor" className="card--img-mentor" />
-          <h2 className="card-title">{name}</h2>
-
-          <p className="card-description">{description}</p>
-        </Link>
-      </div>
-      {/* Lien vers la fiche mentor avec /ressources/+name */}
+  <article className="card-mentor">
+    <div className="card-mentor-content">
+        <div className="card-mentor-info d-flex justify-content-start align-items-center">
+            <div className="card-mentor-info-image">
+                <img src={mentor} alt="mentor" />
+            </div>
+            <div className="card-mentor-info-title">
+                <div className="card-mentor-info-title-name">Hilgeugeu</div>
+                <div className="card-mentor-info-title-role">Dev Front-End</div>
+            </div>
+        </div>
+        <div className="card-mentor-technos">
+            <span className="badge badge-html">html</span>
+            <span className="badge badge-css">css</span>
+            <span className="badge badge-javascript">javascript</span>
+        </div>
     </div>
   </article>
 );
@@ -29,3 +33,19 @@ Card.propTypes = {
 };
 
 export default Card;
+
+
+{/* <article className="card">
+    <div className="card--container">
+      <div className="card-content"> */}
+        {/* Lien vers la fiche mentor avec /ressources/+name */}
+  //       <Link to={`/mentors/${name}`}>
+  //         <img src={image} alt="une de image mentor" className="card--img-mentor" />
+  //         <h2 className="card-title">{name}</h2>
+
+  //         <p className="card-description">{description}</p>
+  //       </Link>
+  //     </div>
+  //     {/* Lien vers la fiche mentor avec /ressources/+name */}
+  //   </div>
+  // </article>
--- front/src/components/Lists/ListRessources/Card/index.js
+++ front/src/components/Lists/ListRessources/Card/index.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles.scss';
-import cardImg from 'src/assets/images/Card_Img.png';
+// import cardImg from 'src/assets/images/Card_Img.png';
 
 const Card = ({ title, description, slug }) => (
-  <article className="card">
-    <div className="card--container">
-      <img src={cardImg} alt="une image" />
-      <div className="card-content">
-        {/* Lien externe d'origine de la ressource sur toute la div article */}
-        {/* <a href={`http://localhost:8080/ressources/${slug}`} target="_blank"> */}
-
-        <h2 className="card-title">{title}</h2>
-        <p className="card-description">{description}</p>
-      </div>
-
-      <div className="card--conButton">
-        {/* Lien vers la fiche ressource avec /slug */}
-        <Link to={`/ressources/${slug}`} className="card-button">
-          En savoir plus
-        </Link>
-      </div>
+  <article className="card-ressource card-ressource-1">
+    <div className="card-ressource-content">
+        <div className="card-ressource-badge">
+            <span className="badge badge-facile">Facile</span>
+        </div>
+        <div className="card-ressource-description">
+            <h3>Apprendre le html par la pratique</h3>
+            <div className="card-ressource-description-details">
+                <p>par <span>Rémi et Thibaud</span></p>
+                <p>Durée - <span>10 min</span></p>
+            </div>
+        </div>
     </div>
-  </article>
+</article>
 );
 
 export default Card;
+
+
+
+{/* <article className="card">
+    <div className="card--container">
+      <img src={cardImg} alt="une image" />
+      <div className="card-content"> */}
+        {/* Lien externe d'origine de la ressource sur toute la div article */}
+        {/* <a href={`http://localhost:8080/ressources/${slug}`} target="_blank"> */}
+
+  //       <h2 className="card-title">{title}</h2>
+  //       <p className="card-description">{description}</p>
+  //     </div>
+
+  //     <div className="card--conButton">
+  //       {/* Lien vers la fiche ressource avec /slug */}
+  //       <Link to={`/ressources/${slug}`} className="card-button">
+  //         En savoir plus
+  //       </Link>
+  //     </div>
+  //   </div>
+  // </article>
